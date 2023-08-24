678ca16f5ee2e27e40695d9759dc2c8c7a4ce924
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-23 20:53:44 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,6 +1,19 @@
 public class Solution {
     public double calculateTax(int[][] brackets, int income) {
-    
-        return 0;
+        int remaining = income;
+        double tax = 0;
+        for (int i = 0; i < brackets.length; i++) {
+            if (remaining <= 0)
+                break;
+            int taxable = (i - 1 >= 0) ? brackets[i][0] - brackets[i - 1][0] : brackets[i][0];
+            if (remaining - taxable >= 0) {
+                remaining -= taxable;
+                tax += taxable * ((double) brackets[i][1] / 100);
+            } else {
+                tax += remaining * ((double) brackets[i][1] / 100);
+                remaining = 0;
+            }
+        }
+        return tax;
     }
 }
